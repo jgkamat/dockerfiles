ce4570c4e9f3c112652cb87b23dfff97dcb493d0
Jay Kamat <jaygkamat@gmail.com>
2014-09-28 00:36:33 -0400

    Fix an automated build issue

--- Owncloud/buildDockerfile.sh
+++ Owncloud/buildDockerfile.sh
@@ -4,10 +4,10 @@
 
 # copy over ssh keys, if not there, make them!
 
-if [ ! -f ~/.ssh/id_rsa.pub -a ! -f "$(dirname $0)/id_rsa.pub" ]; then
-    # Generate a ssh key with no password!
-    ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-fi
-cp ~/.ssh/id_rsa.pub "$(dirname $0)/id_rsa.pub"
+#if [ ! -f ~/.ssh/id_rsa.pub -a ! -f "$(dirname $0)/id_rsa.pub" ]; then
+# Generate a ssh key with no password!
+#ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+#fi
+#cp ~/.ssh/id_rsa.pub "$(dirname $0)/id_rsa.pub"
 
 sudo docker build -t jgkamat/owncloud .
